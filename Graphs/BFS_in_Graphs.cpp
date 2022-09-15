a87348bb270c882e0ee467c1632e02288b1839d7
Lakshay Bhardwaj <91718893+lakshay1121@users.noreply.github.com>
2022-09-15 16:33:20 +0530

    Create BFS_in_Graphs.cpp

---
+++ Graphs/BFS_in_Graphs.cpp
@@ -0,0 +1,36 @@
+ vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        
+        int vis[V] = {0};
+        
+        vis[0] = 1;
+        
+        queue<int> q;
+        
+        q.push(0);
+        
+        vector<int> ans;
+        
+        while(!q.empty()){
+            
+            int node = q.front();
+            
+            q.pop();
+            
+            ans.push_back(node);
+            
+            for(auto it: adj[node]){
+                
+                if(!vis[it]){
+                    
+                    q.push(it);
+                    
+                    vis[it] = 1;
+                    
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
