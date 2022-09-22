6a945f3f27e606fd9d6b6985ac206736d93be331
Lakshay Bhardwaj <91718893+lakshay1121@users.noreply.github.com>
2022-09-22 23:16:34 +0530

    Update set_matrix_zero_with_dummyrow_and_dummycol.cpp

--- Matrix/set_matrix_zero_with_dummyrow_and_dummycol.cpp
+++ Matrix/set_matrix_zero_with_dummyrow_and_dummycol.cpp
@@ -34,5 +34,56 @@ public:
 
         return;
         
+        //time - O(n*m) +)(n*m)
+        //space - O(m) + O(n).
+        
+                
     }
 };
+        
+ //highest optimized.
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        
+      int rowsize = matrix.size() , colsize = matrix[0].size() , col0 = 1;
+        
+      for(int i = 0 ; i < rowsize; i++){
+          
+          if(matrix[i][0] == 0) col0 = 0;
+          
+          for(int j = 1 ; j < colsize; j++){
+              
+              if(matrix[i][j] == 0){
+                  
+                  matrix[i][0] = matrix[0][j] = 0;
+              }
+          }
+      }
+        
+        
+        //iterates backwards.
+        
+        for(int i = rowsize - 1 ; i >= 0 ; i--){
+            
+            for(int j = colsize - 1; j >=1 ; j--){
+                
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                        
+                    matrix[i][j] = 0;
+                }
+            
+                if(col0 == 0){
+                    
+                    matrix[i][0] = 0;
+                }
+        }
+        
+    }
+};
+
+        
+        
+
