da91dc4585c964a33c3b1fc8b0a1b176995accea
Lakshay Bhardwaj <91718893+lakshay1121@users.noreply.github.com>
2023-12-23 15:48:45 +0530

    Create besttimetobuyandsell.cpp

---
+++ Arrays/besttimetobuyandsell.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+       int mini = prices[0];
+       int maxProfit = 0;
+
+       for(int i = 1 ; i < prices.size() ; i++){
+
+           int cost = prices[i] - mini;
+           maxProfit = max(cost , maxProfit);
+           mini = min(mini , prices[i]);
+       }
+
+       return maxProfit;
+        
+    }
+};
